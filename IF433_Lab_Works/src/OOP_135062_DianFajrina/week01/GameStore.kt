744f77764ff1848d111d6e496cb8b6ba88926967
Chozzey <dianfajrinar13@gmail.com>
2026-02-06 23:16:42 +0700

    week01: (tugas 2) implement null safety for user note

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/GameStore.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/GameStore.kt
@@ -3,6 +3,8 @@ package oop_135062_dianfajrina.week01
 fun main() {
     val gameTitle = "Forza Horizon 5 - Standard Edition"
     val price = 699000
+    val userNote: String? = null
+
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
@@ -10,15 +12,22 @@ fun main() {
     printReceipt(
         title = gameTitle,
         originalPrice = price,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        note = userNote
     )
 }
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) price * 20 / 100 else price * 10 / 100
 
-fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+fun printReceipt(
+    title: String,
+    originalPrice: Int,
+    finalPrice: Int,
+    note: String?
+) {
     println("Judul Game : $title")
     println("Harga Asli : Rp $originalPrice")
     println("Harga Akhir: Rp $finalPrice")
+    println("Catatan    : ${note ?: "Tidak ada catatan"}")
 }
