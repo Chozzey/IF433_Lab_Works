d8c83dfaa8e05952e4840f61be0d71220a105108
Chozzey <dianfajrinar13@gmail.com>
2026-02-06 23:13:01 +0700

    week01: (tugas 2) implement discount logic

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/GameStore.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_135062_dianfajrina.week01
 fun main() {
     val gameTitle = "Forza Horizon 5 - Standard Edition"
     val price = 699000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price * 20 / 100 else price * 10 / 100
