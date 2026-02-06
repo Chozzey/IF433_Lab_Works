62d60ec49731d16d3803644aa198bfa957a174c3
Chozzey <dianfajrinar13@gmail.com>
2026-02-06 23:23:46 +0700

    week01: (tugas 1) add legacy CircleCalculator code

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/CircleCalculator.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week01/CircleCalculator.kt
@@ -1,14 +1,20 @@
 package oop_135062_dianfajrina.week01
 
-fun main() {
-    val radius = 7.0
-    val pi = 3.14
+fun main(args: Array<String>) {
+    var radius: Double = 7.0
+    var pi: Double = 3.14
 
-    val area = pi * radius * radius
+    var area: Double = pi * radius * radius
 
-    println("Radius: $radius, Area: $area")
-    println(checkSize(area))
+    println("Radius: " + radius + ", Area: " + area)
+
+    checkSize(area)
 }
 
-fun checkSize(area: Double) =
-    if (area > 100) "This is a Big Circle" else "This is a Small Circle"
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
