6a4c9ba7b551d25f92fb75c5257a4289bef4bea1
Chozzey <dianfajrinar13@gmail.com>
2026-02-15 23:42:50 +0700

    week02: implement selection logic for constructors

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week2/Main.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week2/Main.kt
@@ -12,16 +12,33 @@ fun main() {
 
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-
     scanner.nextLine()
 
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
         print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        val majorInput = scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai!")
+        // Membuat objek pertama (Memicu LOG pertama)
+        val tempStudent = Student(name, nim, majorInput)
+        println("Status: Pendaftaran Selesai.")
+
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
+
+        if (type == 1) {
+            val s1 = Student(name, nim, majorInput)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+        } else if (type == 2) {
+            // Memanggil Secondary Constructor (Memicu LOG alokasi memory & LOG jalur umum)
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
