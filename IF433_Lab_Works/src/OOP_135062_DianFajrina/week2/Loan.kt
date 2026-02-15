b38c4de8b1623e7a45abffce78bb2e528900a892
Chozzey <dianfajrinar13@gmail.com>
2026-02-16 01:07:41 +0700

    week02: (task 1) create Loan class with default args

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week2/Loan.kt
@@ -0,0 +1,17 @@
+package oop_135062_dianfajrina.week2
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1   // Default Argument = 1 hari
+) {
+
+    // Method untuk menghitung denda
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
