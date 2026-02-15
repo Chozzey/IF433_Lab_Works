13624d8ad8bf3cfa5d1f2891b9cad5d4a3282c4b
Chozzey <dianfajrinar13@gmail.com>
2026-02-16 01:18:16 +0700

    week02: (task 2) create Hero class

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week2/Hero.kt
@@ -0,0 +1,28 @@
+package oop_135062_dianfajrina.week2
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100   // Default HP = 100
+) {
+
+    // Method menyerang
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    // Method menerima damage
+    fun takeDamage(damage: Int) {
+        hp -= damage
+
+        // HP tidak boleh minus
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    // Cek apakah hero masih hidup
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
