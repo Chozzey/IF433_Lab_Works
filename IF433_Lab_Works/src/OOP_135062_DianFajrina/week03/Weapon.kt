782ac077271148a56e2f8598ca9eeafc255966e3
Chozzey <dianfajrinar13@gmail.com>
2026-02-20 11:30:24 +0700

    week03: (task 1) create Weapon class with validation

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week03/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_135062_dianfajrina.week03
+
+class Weapon(val name: String, damage: Int) {
+
+    var damage: Int = damage
+        set(value) {
+            if (value < 0) {
+                println("Damage tidak boleh negatif! Nilai tidak berubah.")
+            } else if (value > 1000) {
+                println("Damage terlalu besar! Diset ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
