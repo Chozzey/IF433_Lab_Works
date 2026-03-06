03c80b4f532f32e6bb4454a5be97e144e94179a0
Chozzey <dianfajrinar13@gmail.com>
2026-03-06 11:42:04 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week05/Tugas1_Main.kt
@@ -0,0 +1,11 @@
+package oop_135062_dianfajrina.week05
+
+fun main() {
+
+    val math = MathHelper()
+
+    println("Luas Persegi: " + math.hitungLuas(5))
+    println("Luas Persegi Panjang: " + math.hitungLuas(6, 4))
+    println("Luas Lingkaran: " + math.hitungLuas(7.0))
+
+}
