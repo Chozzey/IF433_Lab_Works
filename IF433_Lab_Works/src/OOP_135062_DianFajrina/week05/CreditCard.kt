27a99093ba49c41812bbb85d88d2afe011d28e38
Chozzey <dianfajrinar13@gmail.com>
2026-03-06 11:51:40 +0700

    week05: (task 2) create CreditCard class

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week05/CreditCard.kt
@@ -0,0 +1,17 @@
+package oop_135062_dianfajrina.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Pembayaran berhasil menggunakan Credit Card oleh $accountName")
+            println("Total terpakai: $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit kartu kredit.")
+        }
+    }
+}
