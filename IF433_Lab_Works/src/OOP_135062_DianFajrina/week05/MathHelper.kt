8d96e921e15ac62c169b6d0d3d375d12063e44ed
Chozzey <dianfajrinar13@gmail.com>
2026-03-06 11:30:56 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week05/MathHelper.kt
@@ -0,0 +1,19 @@
+package oop_135062_dianfajrina.week05
+
+class MathHelper {
+
+    // luas persegi
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    // luas persegi panjang
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    // luas lingkaran
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
