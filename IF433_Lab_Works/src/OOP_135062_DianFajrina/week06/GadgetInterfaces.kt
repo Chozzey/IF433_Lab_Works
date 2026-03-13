1e3c47f5644c165525d03b4c1ed6f4dc8f1d5677
Chozzey <dianfajrinar13@gmail.com>
2026-03-13 11:00:01 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_135062_dianfajrina.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa Kamera Terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("sinyal seluler mencari jaringan")}
+}
