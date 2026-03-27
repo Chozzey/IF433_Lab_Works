3a6ba658ff83af7decdf8c415fd1654983ac04ff
Chozzey <dianfajrinar13@gmail.com>
2026-03-27 10:48:16 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week07/Main.kt
@@ -0,0 +1,3 @@
+package oop_135062_dianfajrina.week07
+
+val client = NetworkClient("https://api.umn.ac.id")
---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week07/NetworkClient.kt
@@ -0,0 +1,7 @@
+package oop_135062_dianfajrina.week07
+
+class NetworkClient {
+    fun connect() {
+        println("Connecting to $url...")
+    }
+}
