af8f9dca40ff1240d70845800d5b6dc48b5524b4
Chozzey <dianfajrinar13@gmail.com>
2026-03-27 15:20:47 +0700

    week07: (task) test GameManager singleton in main

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week07/Main_TugasMandiri.kt
@@ -0,0 +1,35 @@
+package oop_135062_dianfajrina.week07
+
+fun main() {
+    println("\n=== TUGAS MANDIRI: RPG GAME ===")
+
+    // 1. Test Singleton GameManager
+    println("\n--- Game Manager ---")
+    GameManager.startGame()
+    GameManager.startGame() // buktikan singleton
+
+    // 2. Cetak drop chance LEGENDARY
+    println("\n--- Item Rarity Drop Chance ---")
+    println("Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    // 3. Factory Weapon
+    println("\n--- Factory Weapon ---")
+    val starterSword = Weapon.forgeStarterSword()
+    println("Senjata awal: $starterSword")
+
+    val epicSword = Weapon.forgeEpicSword()
+    println("Senjata epic: $epicSword")
+
+    // 4. Upgrade senjata menggunakan copy()
+    println("\n--- Upgrade Senjata ---")
+    val upgradedItem = starterSword.item.copy(damage = 25, name = "Pedang Kayu Upgrade")
+    val upgradedWeapon = Weapon(upgradedItem, starterSword.durability)
+    println("Senjata setelah upgrade: $upgradedWeapon")
+
+    // 5. Simulasi event berurutan
+    println("\n--- Event Battle ---")
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
+}
