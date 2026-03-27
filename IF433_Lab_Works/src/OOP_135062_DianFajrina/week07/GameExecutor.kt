7b27f43d6758f6c41d7c1cba2da92bff1c719767
Chozzey <dianfajrinar13@gmail.com>
2026-03-27 15:17:01 +0700

    week07: (task) implement exhaustive processEvent

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week07/GameExecutor.kt
@@ -0,0 +1,19 @@
+package oop_135062_dianfajrina.week07
+
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> {
+            println("⚔️ Monster muncul: ${event.monsterName}! Bersiaplah bertarung!")
+        }
+        is BattleState.LootDropped -> {
+            val item = event.item
+            println("🎁 Mendapatkan loot: ${item.name} (${item.rarity}) - Damage: ${item.damage}")
+        }
+        is BattleState.GameOver -> {
+            println("💀 GAME OVER: ${event.reason}")
+        }
+        BattleState.SafeZone -> {
+            println("🏠 Anda berada di Safe Zone. Istirahat sejenak.")
+        }
+    }
+}
