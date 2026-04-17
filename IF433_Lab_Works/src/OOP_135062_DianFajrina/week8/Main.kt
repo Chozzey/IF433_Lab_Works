fcd85ea232d29666434649955b1bfa799476c705
Chozzey <dianfajrinar13@gmail.com>
2026-04-17 10:53:20 +0700

    week08: create mixed data collection

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week8/Main.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week8/Main.kt
@@ -14,4 +14,14 @@ fun main() {
         "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
     } ?: "Transaksi Invalid: Harga belum di-set!"
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Tzuyu", null),
+        "Laptop",
+        4500000.0
+    )
+
 }
