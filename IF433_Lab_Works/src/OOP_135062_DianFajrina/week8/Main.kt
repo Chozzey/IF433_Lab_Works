7da5ef9bdfc343452bcf728603dae5d33bc256d1
Chozzey <dianfajrinar13@gmail.com>
2026-04-17 10:49:20 +0700

    week08: implement chained safe calls and elvis

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week8/Main.kt
@@ -0,0 +1,9 @@
+package oop_135062_dianfajrina.week8
+
+fun main() {
+    println("===TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+}
