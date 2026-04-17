4777cd0b538d81f57637485c2360e80aec871399
Chozzey <dianfajrinar13@gmail.com>
2026-04-17 10:50:18 +0700

    week08: implement safe call with let block

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week8/Main.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week8/Main.kt
@@ -1,9 +1,17 @@
 package oop_135062_dianfajrina.week8
 
 fun main() {
-    println("===TEST SAFE CALLS & ELVIS ===")
+    println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
 
-    val destination = emptyOrder.deliveryDetails?.address?.city?.name?: "Kota Tidak Diketahui"
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+    val receipt = validOrder.totalPrice?.let { price ->
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+    println(receipt)
 }
