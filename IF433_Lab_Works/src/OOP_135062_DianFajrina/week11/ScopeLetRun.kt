11c8035f493a61d23807bf8acccd9a94e5ca80f5
Chozzey <dianfajrinar13@gmail.com>
2026-05-08 12:30:50 +0700

    week11: implement let function for null safety

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week11/ScopeLetRun.kt
@@ -0,0 +1,13 @@
+package oop_135062_dianfajrina.week11
+
+fun main() {
+
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it") // 'it' merujuk pada name
+        it.length // Mengembalikan nilai expression terakhir
+    }
+    println("Panjang nama: $length")
+}
