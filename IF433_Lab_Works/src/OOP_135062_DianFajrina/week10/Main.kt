2f6b20b09330ab68a97de40f14cd1e3a689ad030
Chozzey <dianfajrinar13@gmail.com>
2026-05-08 10:30:41 +0700

    week10: test math operations and comparable constraints

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/Main.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/Main.kt
@@ -11,4 +11,9 @@ fun main() {
     println("\n=== TEST MULTIPLE PARAMETERS ===")
     val itemPrice =  PairBox("Bitcoin", 65000)
     println("Aset: ${itemPrice.value}, Harga: ${itemPrice.value} USD")
+    
+    println("\n=== TEST CONSTRAINTS ===")
+    val math = MathBox(10.5, 20) [cite: 90]
+    println("Total: ${math.sum()}") [cite: 91]
+    println("Terbesar: ${getMax(45, 90)}") [cite: 92]
 }
