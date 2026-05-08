dc231372ce0f02b9bc9463f871128594a4e357dd
Chozzey <dianfajrinar13@gmail.com>
2026-05-08 12:05:30 +0700

    week10: (task) populate coin repository

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/CryptoDashboard.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_135062_dianfajrina.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>() [cite: 122, 123]
+    coinRepo.add(Coin("BTC", 0.5)) [cite: 127]
+    coinRepo.add(Coin("ETH", 2.0)) [cite: 127]
+    coinRepo.add(Coin("USDT", 1000.0)) [cite: 127]
 }
