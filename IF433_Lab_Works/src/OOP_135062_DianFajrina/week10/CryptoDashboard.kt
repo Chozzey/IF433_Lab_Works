a2068586c4896dcfbebb49d53c21e37c7578ce18
Chozzey <dianfajrinar13@gmail.com>
2026-05-08 12:05:51 +0700

    week10: (task) wrap repository data into ApiResponse

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/CryptoDashboard.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/CryptoDashboard.kt
@@ -2,7 +2,10 @@ package oop_135062_dianfajrina.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>() [cite: 122, 123]
+    
     coinRepo.add(Coin("BTC", 0.5)) [cite: 127]
     coinRepo.add(Coin("ETH", 2.0)) [cite: 127]
     coinRepo.add(Coin("USDT", 1000.0)) [cite: 127]
+
+    val response = ApiResponse("200 OK", coinRepo.getAll()) [cite: 132]
 }
