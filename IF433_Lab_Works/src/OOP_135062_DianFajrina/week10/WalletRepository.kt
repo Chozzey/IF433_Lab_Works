7e37826337fe66f79a324c7cbd0e8a354debcb52
Chozzey <dianfajrinar13@gmail.com>
2026-05-08 11:44:52 +0700

    week10: (task) implement generic WalletRepository

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week10/WalletRepository.kt
@@ -0,0 +1,17 @@
+package oop_135062_dianfajrina.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+    
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
+    }
+}
