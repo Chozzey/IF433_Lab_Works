c8ac807caf64e47619950759743c326c38be3dbb
Chozzey <dianfajrinar13@gmail.com>
2026-05-15 11:18:49 +0700

    week11: (task) initialize smart home device list

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week11/SmartHomeDashboard.kt
@@ -0,0 +1,7 @@
+package oop_135062_dianfajrina.week11
+
+fun main() {
+
+    val homeDevices = mutableListOf<SmartDevice>()
+
+}
--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week11/SmartHomeModels.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week11/SmartHomeModels.kt
@@ -1,7 +1,5 @@
 package oop_135062_dianfajrina.week11
 
-package oop_NIM_NAMA.week11
-
 data class SmartDevice(
     var name: String,
     var category: String,
