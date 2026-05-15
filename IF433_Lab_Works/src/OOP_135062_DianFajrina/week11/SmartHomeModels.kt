e6dd7778ce5401adfab913b18d2758f3faeb6b5f
Chozzey <dianfajrinar13@gmail.com>
2026-05-15 11:16:32 +0700

    week11: (task) create SmartDevice data class

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week11/SmartHomeModels.kt
@@ -0,0 +1,10 @@
+package oop_135062_dianfajrina.week11
+
+package oop_NIM_NAMA.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
