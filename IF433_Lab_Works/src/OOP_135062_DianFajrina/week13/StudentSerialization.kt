beda90a84e88abc0a753432c92800d654f13a011
Chozzey <dianfajrinar13@gmail.com>
2026-05-23 17:40:55 +0700

    week13: create save and load list functions

--- IF433_Lab_Works/src/OOP_135062_DianFajrina/week13/StudentSerialization.kt
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week13/StudentSerialization.kt
@@ -10,3 +10,16 @@ fun fromCsv(Line: String): Student {
     val parts = Line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
+
+fun saveStudent(student: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") { it.toCsv() })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
+    }
+}
