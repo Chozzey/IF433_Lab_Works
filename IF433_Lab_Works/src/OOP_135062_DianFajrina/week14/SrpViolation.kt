11fb004e5d668d5909ab818ca6835b133c87feea
Chozzey <dianfajrinar13@gmail.com>
2026-05-29 10:57:48 +0700

    week13: implement basic writeText function

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week14/SrpViolation.kt
@@ -0,0 +1,17 @@
+package oop_135062_dianfajrina.week14
+
+data class User(val name:String, val email:String, val age: Int)
+
+class UserManager {
+    fun ValidateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users VALUES ('${user.name}','${user.email}')")
+    }
+
+    fun sendWelcomeEmail(user: User) {
+        println("Sending email to ${user.email}")
+    }
+}
