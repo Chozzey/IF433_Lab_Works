b3228b4225e59f73ca4f14905ce9abd5c7c8f773
Chozzey <dianfajrinar13@gmail.com>
2026-05-29 11:12:30 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ IF433_Lab_Works/src/OOP_135062_DianFajrina/week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_135062_dianfajrina.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double): Double = price * 0.80
+}
+
+class MemberDisconut : DiscountStrategy {
+    override fun apply(price: Double): Double = price * 0.85
+}
